6bad712e338f0e9715976fa1557e67e79a67ec54
sporteiro <seblash@gmail.com>
2026-05-31 09:09:11 +0100

    reviewed styles

--- js/js.js
+++ js/js.js
@@ -1,225 +1,64 @@
 function mostrarVE () {
 		var contenido=document.getElementById('contenido');
-		contenido.innerHTML="<iframe src='vender.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='900px' height='600px'></iframe>";
-		var b1=document.getElementById('b1');
-		b1.style.backgroundColor="black";
-
-		var b2=document.getElementById('b2');
-		b2.style.backgroundColor="blue";
-		
-		var b3=document.getElementById('b3');
-		b3.style.backgroundColor="blue";
-		
-		var b4=document.getElementById('b4');
-		b4.style.backgroundColor="blue";
-		
-		var b5=document.getElementById('b5');
-		b5.style.backgroundColor="blue";
-		
-		var b6=document.getElementById('b6');
-		b6.style.backgroundColor="blue";
-		
-
-		var b7=document.getElementById('b7');
-		b7.style.backgroundColor="blue";
-
-		var b8=document.getElementById('b8');
-		b8.style.backgroundColor="blue";
+		contenido.innerHTML="<iframe src='vender.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='100%' height='600px'></iframe>";
+		setActiveTab('b1');
 }
 
 
 	function mostrarPR () {
 		var contenido=document.getElementById('contenido');
-		contenido.innerHTML="<iframe src='proveedor.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='900px' height='600px'></iframe>";
-		var b2=document.getElementById('b2');
-		b2.style.backgroundColor="black";
-		
-		var b1=document.getElementById('b1');
-		b1.style.backgroundColor="blue";
-		
-		var b3=document.getElementById('b3');
-		b3.style.backgroundColor="blue";
-		
-		var b4=document.getElementById('b4');
-		b4.style.backgroundColor="blue";
-
-		var b5=document.getElementById('b5');
-		b5.style.backgroundColor="blue";
-
-		var b6=document.getElementById('b6');
-		b6.style.backgroundColor="blue";
-
-		var b7=document.getElementById('b7');
-		b7.style.backgroundColor="blue";
-
-		var b8=document.getElementById('b8');
-		b8.style.backgroundColor="blue";
+		contenido.innerHTML="<iframe src='proveedor.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='100%' height='600px'></iframe>";
+		setActiveTab('b2');
 }
 
 	function mostrarAR () {
 		var contenido=document.getElementById('contenido');
-		contenido.innerHTML="<iframe src='articulo.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='900px' height='600px'></iframe>";
-		var b3=document.getElementById('b3');
-		b3.style.backgroundColor="black";
-		
-		var b1=document.getElementById('b1');
-		b1.style.backgroundColor="blue";
-		
-		var b2=document.getElementById('b2');
-		b2.style.backgroundColor="blue";
-		
-		var b4=document.getElementById('b4');
-		b4.style.backgroundColor="blue";
-		
-		var b5=document.getElementById('b5');
-		b5.style.backgroundColor="blue";
-		
-		var b6=document.getElementById('b6');
-		b6.style.backgroundColor="blue";
-
-		var b7=document.getElementById('b7');
-		b7.style.backgroundColor="blue";
-
-		var b8=document.getElementById('b8');
-		b8.style.backgroundColor="blue";
+		contenido.innerHTML="<iframe src='articulo.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='100%' height='600px'></iframe>";
+		setActiveTab('b3');
 }
 
 	function mostrarFA () {
 		var contenido=document.getElementById('contenido');
-		contenido.innerHTML="<iframe src='factura.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='900px' height='600px'></iframe>";
-		var b4=document.getElementById('b4');
-		b4.style.backgroundColor="black";
-		
-		var b1=document.getElementById('b1');
-		b1.style.backgroundColor="blue";
-		
-		var b2=document.getElementById('b2');
-		b2.style.backgroundColor="blue";
-		
-		var b3=document.getElementById('b3');
-		b3.style.backgroundColor="blue";
-	
-		var b5=document.getElementById('b5');
-		b5.style.backgroundColor="blue";
-
-		var b6=document.getElementById('b6');
-		b6.style.backgroundColor="blue";
-
-		var b7=document.getElementById('b7');
-		b7.style.backgroundColor="blue";
-
-		var b8=document.getElementById('b8');
-		b8.style.backgroundColor="blue";
+		contenido.innerHTML="<iframe src='factura.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='100%' height='600px'></iframe>";
+		setActiveTab('b4');
 }
 
 	function mostrarCO() {
 		var contenido=document.getElementById('contenido');
-		contenido.innerHTML="<iframe src='consulta.php' style='overflow-x:hidden; overflow-y:auto;'  frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='900px' height='600px'></iframe>";
-		var b5=document.getElementById('b5');
-		b5.style.backgroundColor="black";
-		
-		var b1=document.getElementById('b1');
-		b1.style.backgroundColor="blue";
-		
-		var b2=document.getElementById('b2');
-		b2.style.backgroundColor="blue";
-		
-		var b3=document.getElementById('b3');
-		b3.style.backgroundColor="blue";
-		
-		var b4=document.getElementById('b4');
-		b4.style.backgroundColor="blue";
-		
-		var b6=document.getElementById('b6');
-		b6.style.backgroundColor="blue";
-
-		var b7=document.getElementById('b7');
-		b7.style.backgroundColor="blue";
-
-		var b8=document.getElementById('b8');
-		b8.style.backgroundColor="blue";
+		contenido.innerHTML="<iframe src='consulta.php' style='overflow-x:hidden; overflow-y:auto;'  frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='100%' height='600px'></iframe>";
+		setActiveTab('b5');
 }
 
 function mostrarCC() {
 		var contenido=document.getElementById('contenido');
-		contenido.innerHTML="<iframe src='comprar.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='900px' height='600px'></iframe>";
-		var b6=document.getElementById('b6');
-		b6.style.backgroundColor="black";
-		
-		var b1=document.getElementById('b1');
-		b1.style.backgroundColor="blue";
-		
-		var b2=document.getElementById('b2');
-		b2.style.backgroundColor="blue";
-		
-		var b3=document.getElementById('b3');
-		b3.style.backgroundColor="blue";
-		
-		var b4=document.getElementById('b4');
-		b4.style.backgroundColor="blue";
-		
-		var b5=document.getElementById('b5');
-		b5.style.backgroundColor="blue";
-		
-		var b7=document.getElementById('b7');
-		b7.style.backgroundColor="blue";
-		
-		var b8=document.getElementById('b8');
-		b8.style.backgroundColor="blue";
+		contenido.innerHTML="<iframe src='comprar.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='100%' height='600px'></iframe>";
+		setActiveTab('b6');
 }
 
 function mostrarCT() {
 		var contenido=document.getElementById('contenido');
-		contenido.innerHTML="<iframe src='contabilidad.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='900px' height='600px'></iframe>";
-		var b7=document.getElementById('b7');
-		b7.style.backgroundColor="black";
-		
-		var b1=document.getElementById('b1');
-		b1.style.backgroundColor="blue";
-		
-		var b2=document.getElementById('b2');
-		b2.style.backgroundColor="blue";
-		
-		var b3=document.getElementById('b3');
-		b3.style.backgroundColor="blue";
-		
-		var b4=document.getElementById('b4');
-		b4.style.backgroundColor="blue";
-		
-		var b5=document.getElementById('b5');
-		b5.style.backgroundColor="blue";
-		
-		var b6=document.getElementById('b6');
-		b6.style.backgroundColor="blue";
-		
-		var b8=document.getElementById('b8');
-		b8.style.backgroundColor="blue";
+		contenido.innerHTML="<iframe src='contabilidad.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='100%' height='600px'></iframe>";
+		setActiveTab('b7');
 }
 
 function mostrarMA() {
 		var contenido=document.getElementById('contenido');
-		contenido.innerHTML="<iframe src='mantenimiento.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='900px' height='600px'></iframe>";
-		var b8=document.getElementById('b8');
-		b8.style.backgroundColor="black";
-		
-		var b1=document.getElementById('b1');
-		b1.style.backgroundColor="blue";
-		
-		var b2=document.getElementById('b2');
-		b2.style.backgroundColor="blue";
-		
-		var b3=document.getElementById('b3');
-		b3.style.backgroundColor="blue";
-		
-		var b4=document.getElementById('b4');
-		b4.style.backgroundColor="blue";
-		
-		var b5=document.getElementById('b5');
-		b5.style.backgroundColor="blue";
-		
-		var b6=document.getElementById('b6');
-		b6.style.backgroundColor="blue";
-		
-		var b7=document.getElementById('b7');
-		b7.style.backgroundColor="blue";
+		contenido.innerHTML="<iframe src='mantenimiento.php' style='overflow-x:hidden; overflow-y:auto;' frameborder='0' scrolling='yes' marginheight='0' marginwidth='0' width='100%' height='600px'></iframe>";
+		setActiveTab('b8');
+}
+
+function setActiveTab(activeId) {
+	// Remove active class from all tabs
+	var tabs = ['b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8'];
+	for (var i = 0; i < tabs.length; i++) {
+		var tab = document.getElementById(tabs[i]);
+		if (tab) {
+			tab.classList.remove('active');
+		}
+	}
+	// Add active class to the selected tab
+	var activeTab = document.getElementById(activeId);
+	if (activeTab) {
+		activeTab.classList.add('active');
+	}
 }
